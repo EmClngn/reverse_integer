015ba2daf98fd30a0c59dfcb854172c2f54211c5
unknown <emgcalangian@gmail.com>
2024-01-24 22:31:00 +0800

    pseudo code

---
+++ reverse_integer.py
@@ -0,0 +1,7 @@
+# Write a Program to extract each digit from an integer in the reverse order with a space separating them
+
+# pseudo code
+
+# ask user for number input
+
+# use for loop to reverse the numbers
